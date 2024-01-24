427f58d0a0467e4e804a4c65c9743cee439900bb
ChillerDragon <ChillerDragon@gmail.com>
2024-01-24 21:41:09 +0800

    first api only prototype

--- README.md
+++ README.md
@@ -10,3 +10,32 @@ Paste your raw teeworlds traffic as hex and get annotations about which package 
     FLASK_ENV=development FLASK_APP=main.py flask run
 
 Then goto http://localhost:5000/ in your browser
+
+## api usage
+
+```
+$ curl -s http://127.0.0.1:5000/api/v1/decode/ff -X POST | jq .message -r
+[twnet_parser v0.8.0][huffman=rust-libtw2] udp payload: ff
+--- 0.7
+Traceback (most recent call last):
+  File "/home/chiller/Desktop/git/teeworlds-web-traffic-decoder/main.py", line 89, in hex_str_to_annotation
+    packet = twnet_parser.packet.parse7(data)
+  File "/home/chiller/Desktop/git/teeworlds-web-traffic-decoder/venv/lib/python3.10/site-packages/twnet_parser/packet.py", line 472, in parse7
+    return PacketParser().parse7(data, we_are_a_client)
+  File "/home/chiller/Desktop/git/teeworlds-web-traffic-decoder/venv/lib/python3.10/site-packages/twnet_parser/packet.py", line 453, in parse7
+    ctrl_msg: CtrlMessage = match_control7(data[header_size], data[8:], client)
+IndexError: index out of range
+
+--- 0.6
+Traceback (most recent call last):
+  File "/home/chiller/Desktop/git/teeworlds-web-traffic-decoder/main.py", line 96, in hex_str_to_annotation
+    packet = twnet_parser.packet.parse6(data)
+  File "/home/chiller/Desktop/git/teeworlds-web-traffic-decoder/venv/lib/python3.10/site-packages/twnet_parser/packet.py", line 469, in parse6
+    return PacketParser().parse6(data, we_are_a_client)
+  File "/home/chiller/Desktop/git/teeworlds-web-traffic-decoder/venv/lib/python3.10/site-packages/twnet_parser/packet.py", line 428, in parse6
+    connless_msg: ConnlessMessage = match_connless6(data[header_size:14], data[14:])
+  File "/home/chiller/Desktop/git/teeworlds-web-traffic-decoder/venv/lib/python3.10/site-packages/twnet_parser/msg_matcher/connless6.py", line 58, in match_connless6
+    f"Error: unknown conless  message id={msg_id!r} data={data[0]}"
+IndexError: index out of range
+
+```
--- main.py
+++ main.py
@@ -3,13 +3,104 @@
 from flask import Flask
 from flask import render_template
 
+import sys
+import time
+import traceback
+
+import re
+import ast
+from binascii import hexlify
+import string
+from importlib.metadata import version
+
+import dpkt
+import twnet_parser.packet
+import twnet_parser.huffman
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html.j2', foo = "bar")
 
+def str_to_bytes(data):
+    data = data.strip()
+    if re.match('^b[\'\"].*[\'\"]$', data):
+        return ast.literal_eval(data)
+
+    if re.match('^0x', data):
+        data = data.replace('0x', '')
+
+    data = bytes(bytearray.fromhex(data))
+    return data
+
+def twpacket_to_str(packet):
+    messages = []
+    messages.append(packet.header)
+    for msg in packet.messages:
+        messages.append(msg)
+    return '\n'.join(messages)
+
+def hex_str_to_annotation(hex_str):
+    # data = """  4500 0035 1aeb 4000 4011 21cb 7f00 0001
+    #   7f00 0001 f367 206f 0021 fe34 100c 0142
+    #     780d 8855 e9f0 87e6 0768 d6d0 5bf8 692f
+    #       ff8c 1437 00
+    #       """
+    # print(str_to_bytes(data).hex(sep = " ")) == "45 00 00 35 1a eb 40 00 40 11 21 cb 7f 00 00 01 7f 00 00 01 f3 67 20 6f 00 21 fe 34 10 0c 01 42 78 0d 88 55 e9 f0 87 e6 07 68 d6 d0 5b f8 69 2f ff 8c 14 37 00"
+    # data = r"b'\x04\x00\x011\xe4\xc3\xd6\x00\x19\x030.7 802f1be60a05665f\x00\x00\x85\x1c'"
+    # print(str_to_bytes(data))
+    # data = "02 7e 01 48 1f 93 d7 40 10 0a 80 01 6f 70 74 69 6f 6e 00 74 65 73 74 00 00 00"
+    # print(str_to_bytes(data))
+    # data = "0x02 0xff 0x03"
+    # print(str_to_bytes(data))
+
+    messages = []
+
+    try:
+        data = str_to_bytes(hex_str)
+    except ValueError:
+        return 'invalid hex'
+
+    udp_payload = data
+    try:
+        ip = dpkt.ethernet.Ethernet(data).data
+        if not isinstance(ip.data, dpkt.udp.UDP):
+            raise ValueError("not udp")
+        udp_payload = ip.data.data
+        messages.append("extracting udp payload from ethernet packet ...")
+    except:
+        pass
+    try:
+        ip = dpkt.ip.IP(data)
+        if not isinstance(ip.data, dpkt.udp.UDP):
+            raise ValueError("not udp")
+        udp_payload = ip.data.data
+        messages.append("extracting udp payload from ip packet ...")
+    except:
+        pass
+
+    data = udp_payload
+
+    messages.append(f"[twnet_parser v{version('twnet_parser')}][huffman={twnet_parser.huffman.backend_name()}] udp payload: {data.hex(sep = ' ')}")
+
+    messages.append("--- 0.7")
+    try:
+        packet = twnet_parser.packet.parse7(data)
+        messages.append(twpacket_to_str(packet))
+    except Exception:
+        messages.append(traceback.format_exc())
+
+    messages.append("--- 0.6")
+    try:
+        packet = twnet_parser.packet.parse6(data)
+        messages.append(twpacket_to_str(packet))
+    except Exception:
+        messages.append(traceback.format_exc())
+
+    return '\n'.join(messages)
+
 @app.route('/api/v1/decode/<string:packet>', methods=["POST"])
-def api_set_gametile(packet):
+def api_decode(packet):
     app.logger.info(f"got packet: {packet}")
-    return {'status': 'ok'}
+    return {'message': hex_str_to_annotation(packet)}
