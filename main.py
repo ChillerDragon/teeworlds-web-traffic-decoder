293c7f34687b8a37456aeddc3f6a51c0eddefc1e
ChillerDragon <ChillerDragon@gmail.com>
2024-02-19 14:06:42 +0800

    Improve logs

--- main.py
+++ main.py
@@ -9,13 +9,19 @@ import src.tw_packet_decoder
 
 app = Flask(__name__)
 
+def req_ipaddr() -> str:
+    print(request)
+    if not 'HTTP_X_FORWARDED_FOR' in request.environ:
+        return request.remote_addr
+    return request.environ['HTTP_X_FORWARDED_FOR'].split(',')[-1].strip()
+
 @app.route('/')
 def index():
     return render_template('index.html.j2', foo = "bar")
 
 @app.route('/api/v1/decode/<string:packet>', methods=["POST"])
 def api_decode_url(packet):
-    app.logger.info(f"got packet via url: {packet}")
+    app.logger.info(f"{req_ipaddr()} send payload via url: {packet}")
     return {'message': src.tw_packet_decoder.hex_str_to_annotation(packet)}
 
 @app.route('/api/v1/decode', methods=["POST"])
@@ -23,5 +29,5 @@ def api_decode_form():
     packet = request.form['data']
     if not packet:
         return {'error': 'data can not be empty'}
-    app.logger.info(f"got packet via form: {packet}")
+    app.logger.info(f"{req_ipaddr()} send payload via form: {packet}")
     return {'message': src.tw_packet_decoder.hex_str_to_annotation(packet)}
