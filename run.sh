c7dddbf8f26420166f4d4b214313e61d9e871f07
ChillerDragon <ChillerDragon@gmail.com>
2024-02-19 15:06:25 +0800

    I cba to figure out how pyson intended logfiles

--- run.sh
+++ run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+mkdir -p logs
+
 source venv/bin/activate || exit 1
 pip install -r requirements.txt || exit 1
 
-gunicorn -w 4 -b 127.0.0.1:9822 'main:app'
+logfile=./logs/gunicorn_"$(date '+%F_%H-%M')".txt
 
+echo "logging to $logfile"
+
+gunicorn \
+	--workers 4 \
+	--bind 127.0.0.1:9822 \
+	--log-level=info \
+	'main:app' &> "$logfile"
+
+	# --log-file=./logs/gunicorn.error.log \
+	# --access-logfile=./logs/gunicorn.access.log \
