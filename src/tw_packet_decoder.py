6d7ac6dfab9b9050ab56eea7da23544808c81e25
ChillerDragon <ChillerDragon@gmail.com>
2024-06-17 11:13:19 +0800

    Support trailing commas in 0x hex

--- src/tw_packet_decoder.py
+++ src/tw_packet_decoder.py
@@ -18,6 +18,7 @@ def str_to_bytes(data: str) -> bytes:
         return ast.literal_eval(data)
 
     data = re.sub(r',\s*0x', '', data)
+    data = re.sub(r',\s*$', '', data)
 
     if re.match('^0x', data):
         data = data.replace('0x', '')
